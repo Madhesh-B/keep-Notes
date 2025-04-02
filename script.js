c043b441126dbc8da7e8d7fad3c7fa3a7fa3af45
Madhesh B <madhesh5463@gmail.com>
2025-04-02 05:45:25 +0530

    file uploaded
    This is the first upload of the file

---
+++ script.js
@@ -0,0 +1,10 @@
+var container = document.getElementById("source");
+var more_btn = document.getElementById("more-icon");
+var options = document.getElementById("more-options-bar");
+
+more_btn.addEventListener("click" , function(){
+    // options.style.width = "175px";
+    // options.style.height = "100px";
+    options.style.display = "block";
+})
+
