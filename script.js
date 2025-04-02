b81b038136e2dceefc71b80e426662f86949840a
Madhesh B <madhesh5463@gmail.com>
2025-04-02 20:25:48 +0530

    Adding delete functionality in js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var container = document.getElementById("source");
 var more_btn = document.getElementById("more-icon");
-var options = document.getElementById("more-options-bar");
+var options = document.getElementById("more-option-bar");
+var btn_div = document.getElementById("icon");
+var del_btn = document.getElementById("delete");
+var li = document.getElementById("first-note");
 
 more_btn.addEventListener("click" , function(){
-    // options.style.width = "175px";
-    // options.style.height = "100px";
     options.style.display = "block";
-})
+});
+
+del_btn.addEventListener("click" , function(){
+    li.remove();
+});
 
